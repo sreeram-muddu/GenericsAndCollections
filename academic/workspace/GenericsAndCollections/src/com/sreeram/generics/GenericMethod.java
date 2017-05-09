bebab1607499bbd13a81c3f43b637cfadf799b31
smuddu <sreeram.muddu@priceline.com>
2017-05-09 14:15:02 -0700

    Passing Array as parameter of Generic type method

--- academic/workspace/GenericsAndCollections/src/com/sreeram/generics/GenericMethod.java
+++ academic/workspace/GenericsAndCollections/src/com/sreeram/generics/GenericMethod.java
@@ -6,6 +6,14 @@ class Main
 	{
 		return t;
 	}
+	
+	public static <T> void showArray(T[] t)
+	{
+		for(T item:t)
+		{
+			System.out.print(" "+item);
+		}
+	}
 }
 
 public class GenericMethod {
@@ -13,8 +21,16 @@ public class GenericMethod {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(Main.generic("muddu"));
-		System.out.println(Main.generic(24));
+//		System.out.println(Main.generic("muddu"));
+//		System.out.println(Main.generic(24));
+		
+		Integer[] integerarray = {2,1,3,4,5};
+		
+		Main.showArray(integerarray);
+		
+		String[] stringarray = {"muddu","sreeram","all","should","be","good."};
+		
+		Main.showArray(stringarray);
 
 	}
 
